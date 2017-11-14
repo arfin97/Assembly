0f663ce9e50594895bc00a99987261151b9982e9
arfin97 <arfin97@gmail.com>
2017-11-14 19:30:28 +0600

    Create 8. Hex to decimal (A,B,C -> 0,1,2).asm

---
+++ 8. Hex to decimal (A,B,C -> 0,1,2).asm
@@ -0,0 +1,49 @@
+; Write· a program to read one of the hex digits A-F, and display it on the next line in decimal.
+.model small
+.stack 100h
+.data
+msg1 db 'enter a hex digit: $'
+msg2 db 'the decimal is: $'
+.code                   
+main proc        
+    ;initialization
+    mov ax, @data
+    mov ds, ax
+    
+    ;display msg 1
+    lea dx, msg1
+    mov ah, 9
+    int 21h
+    
+    ;input hex char
+    mov ah, 1
+    int 21h   
+    
+    ;convert it to decimal
+    mov bl, al
+    sub bl, 17d
+    
+    ;print new line
+    mov ah, 2
+    mov dl, 0dh
+    int 21h
+    mov dl, 0ah
+    int 21h  
+    
+    ;display msg 2
+    lea dx, msg2
+    mov ah, 9
+    int 21h
+    
+    ;display converted num
+    mov ah, 2
+    mov dl, bl
+    int 21h
+    
+    ;exit dos
+    mov ah, 4ch
+    int 21h
+    
+    main endp
+end main
+    
